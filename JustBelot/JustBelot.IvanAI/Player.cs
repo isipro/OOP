47df54cd51707d7cbbcd1bc0ac39df30297c7e7a
isipro <isipro@gmail.com>
2013-03-03 22:47:04 +0200

    List in Program.cs - created

---
+++ JustBelot/JustBelot.IvanAI/Player.cs
@@ -0,0 +1,23 @@
+﻿using JustBelot.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace JustBelot.IvanAI
+{
+    public class Player : IPlayer
+    {
+        public string Name
+        {
+            get { return "Ivan Bot"; }
+        }
+
+        public void PlayedCard()
+        {
+            Console.WriteLine("Ivan played a card");
+        }
+    }
+}
